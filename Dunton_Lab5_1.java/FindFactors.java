109dfd0b9466de64fe4b4b16d06ead76d3edfdd3
RgrUp <160432024+RgrUp@users.noreply.github.com>
2025-02-07 16:46:40 -0500

    Closed Input and added Comments
    Closed the Input to clear the error.
    Added Comments in the code to explain more of what is happening.

--- Dunton_Lab5_1.java/FindFactors.java
+++ Dunton_Lab5_1.java/FindFactors.java
@@ -13,11 +13,14 @@ public class FindFactors {
         System.out.print("Enter the Number to Factor:");
         int Number = input.nextInt();
 
+        // Preemptive print of output before calculations are done.
         System.out.print("The Smallest Factors of " + Number + ": " );
+        // Using While loops to detect if the Number is divisible
+        // by 2,3,5,7,11,13.
         int Test1 = Number; {
         while ( Test1 % 2 ==0 ) {
-
                 Test1 = Test1 / 2 ;
+        // differentiate the output of the loop cycle w/ If Else Statments
                 if (Test1 > 1)
                 System.out.print("2, ");
             else
@@ -26,6 +29,7 @@ public class FindFactors {
         }
         while (Test1 % 3 ==0 ) {
             Test1 = Test1 / 3;
+        // differentiate the output of the loop cycle w/ If Else Statments
             if (Test1 > 1)
                 System.out.print("3, ");
             else
@@ -33,6 +37,7 @@ public class FindFactors {
         }
         while (Test1 % 5 ==0 ) {
             Test1 = Test1 / 5;
+        // differentiate the output of the loop cycle w/ If Else Statments
             if (Test1 > 1)
                 System.out.print("5, ");
             else
@@ -40,6 +45,7 @@ public class FindFactors {
         }
         while (Test1 % 7 ==0 ) {
             Test1 = Test1 / 7;
+        // differentiate the output of the loop cycle w/ If Else Statments
             if (Test1 > 1)
                 System.out.print("7, ");
             else
@@ -47,6 +53,7 @@ public class FindFactors {
         }
         while (Test1 % 11 ==0 ) {
             Test1 = Test1 / 11;
+        // differentiate the output of the loop cycle w/ If Else Statments
             if (Test1 > 1)
                 System.out.print("11, ");
             else
@@ -54,12 +61,16 @@ public class FindFactors {
         }
         while (Test1 % 13 ==0 ) {
             Test1 = Test1 / 13;
+        // differentiate the output of the loop cycle w/ If Else Statments
             if (Test1 > 1)
                 System.out.print("13, ");
             else
                 System.out.print("13.");
         }
+        // Check if the remaining value in Test1 < 1 and if True Print Value to screen.
         if (Test1 > 1)
             System.out.print(" " + Test1 + ".");
+    // Close the input
+    input.close();
     }
 }
