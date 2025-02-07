dbe1d0d1efd0fed84f56c871a157533e48154f18
RgrUp <160432024+RgrUp@users.noreply.github.com>
2025-02-07 14:28:41 -0500

    started new Lab assignment
    Started Lab 5.1 in Java Programming

---
+++ ITS-250/Labs/Dunton_Lab5_1.java/Dunton_Lab5_1.java
@@ -0,0 +1,10 @@
+package Dunton_Lab5_1.java;
+
+public class Dunton_Lab5_1 {
+    public static void main(String[] args) {
+        // This Program will find the factors of an Interger
+        //(smallest factor first and increasing until all factors are listed).
+        // ex: input of 120 outputs : 2,2,2,3,5
+    
+    }
+}
