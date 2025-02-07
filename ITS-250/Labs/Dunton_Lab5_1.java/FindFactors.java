c6e94f60ef60e70b5ea80f1f76e0d8f746b96d77
RgrUp <160432024+RgrUp@users.noreply.github.com>
2025-02-07 14:35:03 -0500

    Fixed file names

--- ITS-250/Labs/Dunton_Lab5_1.java/FindFactors.java
+++ ITS-250/Labs/Dunton_Lab5_1.java/FindFactors.java
@@ -1,10 +1,13 @@
-package Dunton_Lab5_1.java;
+import java.util.Scanner;
 
-public class Dunton_Lab5_1 {
+public class FindFactors {
     public static void main(String[] args) {
         // This Program will find the factors of an Interger
         //(smallest factor first and increasing until all factors are listed).
         // ex: input of 120 outputs : 2,2,2,3,5
     
+        // create a Scanner object for user input
+        Scanner input = new Scanner(System.in);
+
     }
 }
